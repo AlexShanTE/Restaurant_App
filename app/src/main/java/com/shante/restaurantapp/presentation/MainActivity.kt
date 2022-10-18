8144c634ec9e38a8ac79de96fef74e8b283c07dc
Alex <alexsid.ShanTE@gmail.com>
2022-10-18 21:50:34 +0300

    added base navigation with bottom navigation view

--- app/src/main/java/com/shante/restaurantapp/presentation/MainActivity.kt
+++ app/src/main/java/com/shante/restaurantapp/presentation/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.shante.restaurantapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.shante.restaurantapp.R
+import com.shante.restaurantapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val bottomNavigationView = binding.bottomNavigationView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
